2d4e41e080f12c2baa50bc90e47cbf8a0c6d58af
TurKLoJeN <32311900+TurKLoJeN@users.noreply.github.com>
2021-09-28 02:22:10 +0300

    Project File
    source code

---
+++ TurKLoJeN Whois V1.0/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace TurKLoJeN_Whois_V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.iTalk_ThemeContainer1 = new iTalk.iTalk_ThemeContainer();
+            this.iTalk_Button_13 = new iTalk.iTalk_Button_1();
+            this.iTalk_Button_12 = new iTalk.iTalk_Button_1();
+            this.iTalk_Button_21 = new iTalk.iTalk_Button_2();
+            this.iTalk_Button_11 = new iTalk.iTalk_Button_1();
+            this.iTalk_RichTextBox1 = new iTalk.iTalk_RichTextBox();
+            this.iTalk_Label1 = new iTalk.iTalk_Label();
+            this.iTalk_TextBox_Small1 = new iTalk.iTalk_TextBox_Small();
+            this.iTalk_Label2 = new iTalk.iTalk_Label();
+            this.iTalk_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // iTalk_ThemeContainer1
+            // 
+            this.iTalk_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label2);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_13);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_12);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_21);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Button_11);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_RichTextBox1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_Label1);
+            this.iTalk_ThemeContainer1.Controls.Add(this.iTalk_TextBox_Small1);
+            this.iTalk_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iTalk_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_ThemeContainer1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.iTalk_ThemeContainer1.Name = "iTalk_ThemeContainer1";
+            this.iTalk_ThemeContainer1.Padding = new System.Windows.Forms.Padding(3, 28, 3, 28);
+            this.iTalk_ThemeContainer1.Sizable = true;
+            this.iTalk_ThemeContainer1.Size = new System.Drawing.Size(596, 587);
+            this.iTalk_ThemeContainer1.SmartBounds = false;
+            this.iTalk_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.iTalk_ThemeContainer1.TabIndex = 0;
+            this.iTalk_ThemeContainer1.Text = "TurKLoJeN Whois V1.0";
+            // 
+            // iTalk_Button_13
+            // 
+            this.iTalk_Button_13.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_13.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.iTalk_Button_13.Image = null;
+            this.iTalk_Button_13.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_13.Location = new System.Drawing.Point(307, 518);
+            this.iTalk_Button_13.Name = "iTalk_Button_13";
+            this.iTalk_Button_13.Size = new System.Drawing.Size(113, 28);
+            this.iTalk_Button_13.TabIndex = 6;
+            this.iTalk_Button_13.Text = "Copy to Clipboard";
+            this.iTalk_Button_13.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_13.Click += new System.EventHandler(this.iTalk_Button_13_Click);
+            // 
+            // iTalk_Button_12
+            // 
+            this.iTalk_Button_12.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_12.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.iTalk_Button_12.Image = null;
+            this.iTalk_Button_12.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_12.Location = new System.Drawing.Point(160, 518);
+            this.iTalk_Button_12.Name = "iTalk_Button_12";
+            this.iTalk_Button_12.Size = new System.Drawing.Size(113, 28);
+            this.iTalk_Button_12.TabIndex = 5;
+            this.iTalk_Button_12.Text = "Save To File";
+            this.iTalk_Button_12.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_12.Click += new System.EventHandler(this.iTalk_Button_12_Click);
+            // 
+            // iTalk_Button_21
+            // 
+            this.iTalk_Button_21.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_21.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.iTalk_Button_21.ForeColor = System.Drawing.Color.White;
+            this.iTalk_Button_21.Image = null;
+            this.iTalk_Button_21.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_21.Location = new System.Drawing.Point(565, 0);
+            this.iTalk_Button_21.Name = "iTalk_Button_21";
+            this.iTalk_Button_21.Size = new System.Drawing.Size(31, 26);
+            this.iTalk_Button_21.TabIndex = 4;
+            this.iTalk_Button_21.Text = "X";
+            this.iTalk_Button_21.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_21.Click += new System.EventHandler(this.iTalk_Button_21_Click);
+            // 
+            // iTalk_Button_11
+            // 
+            this.iTalk_Button_11.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Button_11.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.iTalk_Button_11.Image = null;
+            this.iTalk_Button_11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iTalk_Button_11.Location = new System.Drawing.Point(471, 58);
+            this.iTalk_Button_11.Name = "iTalk_Button_11";
+            this.iTalk_Button_11.Size = new System.Drawing.Size(113, 28);
+            this.iTalk_Button_11.TabIndex = 3;
+            this.iTalk_Button_11.Text = "SCAN";
+            this.iTalk_Button_11.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.iTalk_Button_11.Click += new System.EventHandler(this.iTalk_Button_11_Click);
+            // 
+            // iTalk_RichTextBox1
+            // 
+            this.iTalk_RichTextBox1.AutoWordSelection = false;
+            this.iTalk_RichTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_RichTextBox1.Enabled = false;
+            this.iTalk_RichTextBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.iTalk_RichTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.iTalk_RichTextBox1.Location = new System.Drawing.Point(17, 108);
+            this.iTalk_RichTextBox1.Name = "iTalk_RichTextBox1";
+            this.iTalk_RichTextBox1.ReadOnly = false;
+            this.iTalk_RichTextBox1.Size = new System.Drawing.Size(567, 397);
+            this.iTalk_RichTextBox1.TabIndex = 2;
+            this.iTalk_RichTextBox1.WordWrap = true;
+            // 
+            // iTalk_Label1
+            // 
+            this.iTalk_Label1.AutoSize = true;
+            this.iTalk_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label1.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.iTalk_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(142)))), ((int)(((byte)(142)))));
+            this.iTalk_Label1.Location = new System.Drawing.Point(12, 58);
+            this.iTalk_Label1.Name = "iTalk_Label1";
+            this.iTalk_Label1.Size = new System.Drawing.Size(183, 25);
+            this.iTalk_Label1.TabIndex = 1;
+            this.iTalk_Label1.Text = "Domain / Hostname: ";
+            // 
+            // iTalk_TextBox_Small1
+            // 
+            this.iTalk_TextBox_Small1.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_TextBox_Small1.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.iTalk_TextBox_Small1.ForeColor = System.Drawing.Color.Silver;
+            this.iTalk_TextBox_Small1.Location = new System.Drawing.Point(201, 58);
+            this.iTalk_TextBox_Small1.MaxLength = 32767;
+            this.iTalk_TextBox_Small1.Multiline = false;
+            this.iTalk_TextBox_Small1.Name = "iTalk_TextBox_Small1";
+            this.iTalk_TextBox_Small1.ReadOnly = false;
+            this.iTalk_TextBox_Small1.Size = new System.Drawing.Size(258, 28);
+            this.iTalk_TextBox_Small1.TabIndex = 0;
+            this.iTalk_TextBox_Small1.Text = "turklojenofficial.com";
+            this.iTalk_TextBox_Small1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.iTalk_TextBox_Small1.UseSystemPasswordChar = false;
+            this.iTalk_TextBox_Small1.Enter += new System.EventHandler(this.iTalk_TextBox_Small1_Enter);
+            this.iTalk_TextBox_Small1.Leave += new System.EventHandler(this.iTalk_TextBox_Small1_Leave);
+            // 
+            // iTalk_Label2
+            // 
+            this.iTalk_Label2.AutoSize = true;
+            this.iTalk_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.iTalk_Label2.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.iTalk_Label2.ForeColor = System.Drawing.Color.Yellow;
+            this.iTalk_Label2.Location = new System.Drawing.Point(454, 570);
+            this.iTalk_Label2.Name = "iTalk_Label2";
+            this.iTalk_Label2.Size = new System.Drawing.Size(132, 13);
+            this.iTalk_Label2.TabIndex = 7;
+            this.iTalk_Label2.Text = "Developed By TurKLoJeN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 587);
+            this.Controls.Add(this.iTalk_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(126, 39);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TurKLoJeN Whois V1.0";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.iTalk_ThemeContainer1.ResumeLayout(false);
+            this.iTalk_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private iTalk.iTalk_ThemeContainer iTalk_ThemeContainer1;
+        private iTalk.iTalk_Button_1 iTalk_Button_11;
+        private iTalk.iTalk_RichTextBox iTalk_RichTextBox1;
+        private iTalk.iTalk_Label iTalk_Label1;
+        private iTalk.iTalk_TextBox_Small iTalk_TextBox_Small1;
+        private iTalk.iTalk_Button_2 iTalk_Button_21;
+        private iTalk.iTalk_Button_1 iTalk_Button_13;
+        private iTalk.iTalk_Button_1 iTalk_Button_12;
+        private iTalk.iTalk_Label iTalk_Label2;
+    }
+}
+
---
+++ TurKLoJeN Whois V1.0/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TurKLoJeN_Whois_V1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void iTalk_Button_11_Click(object sender, EventArgs e)
+        {
+            // richTextBox1.SelectedText.Replace(richTextBox1.SelectedText, "");
+            iTalk_RichTextBox1.Text = "";
+            string siteUrl = iTalk_TextBox_Small1.Text;
+            if (siteUrl == "")
+            {
+                MessageBox.Show("Lütfen bir url giriniz");
+            }
+            else
+            {
+                string txtResponse = String.Empty;
+                string strResponse = String.Empty;
+                TcpClient tcpWhois = new TcpClient("whois.internic.net", 43);
+                NetworkStream nsWhois = tcpWhois.GetStream();
+                BufferedStream bfWhois = new BufferedStream(nsWhois);
+                StreamWriter swSend = new StreamWriter(bfWhois);
+                swSend.WriteLine(siteUrl);
+                swSend.Flush();
+                StreamReader srReceive = new StreamReader(bfWhois);
+                while ((strResponse = srReceive.ReadLine()) != null)
+                {
+                    txtResponse += strResponse + "\r\n";
+                }
+                tcpWhois.Close();
+                iTalk_RichTextBox1.Text = txtResponse;
+            }
+            
+        }
+
+        private void iTalk_Button_21_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void iTalk_Button_12_Click(object sender, EventArgs e)
+        {
+
+            TextWriter writer = new StreamWriter("domain.txt");
+
+            writer.Write(iTalk_RichTextBox1.Text);
+
+            writer.Close();
+
+            MessageBox.Show("File saved successfully");
+        }
+
+        private void iTalk_Button_13_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(iTalk_RichTextBox1.Text);
+            MessageBox.Show("Data successfully copied to clipboard");
+        }
+
+        private void iTalk_TextBox_Small1_Enter(object sender, EventArgs e)
+        {
+            if (iTalk_TextBox_Small1.Text == "turklojenofficial.com")
+            {
+                iTalk_TextBox_Small1.Text = "";
+
+                iTalk_TextBox_Small1.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void iTalk_TextBox_Small1_Leave(object sender, EventArgs e)
+        {
+            if (iTalk_TextBox_Small1.Text == "")
+            {
+                iTalk_TextBox_Small1.Text = "turklojenofficial.com";
+
+                iTalk_TextBox_Small1.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
